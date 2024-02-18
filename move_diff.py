4cd83ffbd7c23567d13426cf724263164648eb9d
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-02-18 01:09:24 +0100

    initial commit

---
+++ move_diff.py
@@ -0,0 +1,30 @@
+class MoveDiffWithFile:
+
+    name = ["MOVE_FILE_WITH_DIFF"]
+
+    def on_file_moved_to_other_project(self,
+        old_filename,
+        new_filename):
+        print('RUNNING')
+
+        old_history_file = os.path.join(
+            os.path.dirname(old_filename), 
+            '_diff',
+            os.path.basename(old_filename + '.diff'))
+        
+        if os.path.exists(old_history_file):
+            if not os.path.exists(os.path.join(
+                    os.path.dirname(new_filename),
+                    '_diff'
+                    )):
+                os.mkdir(
+                    os.path.join(
+                        os.path.dirname(new_filename), 
+                        '_diff'))
+            new_history_file = os.path.join(
+                os.path.dirname(new_filename),
+                '_diff',
+                os.path.basename(new_filename) + '.diff')
+            os.rename(old_history_file, new_history_file)
+
+urtext_extensions=[MoveDiffWithFile]
