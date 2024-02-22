b7060a07411bb57c6f97c34095ebd131b7018fd4
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-02-22 21:06:11 +0100

    remove additional adding of .diff extension; code style changes; remove console logging

--- move_diff.py
+++ move_diff.py
@@ -7,22 +7,19 @@ class MoveDiffWithFile:
     def on_file_moved_to_other_project(self,
         old_filename,
         new_filename):
-        print('RUNNING')
 
         old_history_file = os.path.join(
             os.path.dirname(old_filename), 
             '_diff',
-            os.path.basename(old_filename + '.diff'))
+            os.path.basename(old_filename))
         
         if os.path.exists(old_history_file):
             if not os.path.exists(os.path.join(
-                    os.path.dirname(new_filename),
-                    '_diff'
-                    )):
+                os.path.dirname(new_filename),
+                '_diff'
+                )):
                 os.mkdir(
-                    os.path.join(
-                        os.path.dirname(new_filename), 
-                        '_diff'))
+                    os.path.join(os.path.dirname(new_filename), '_diff'))
             new_history_file = os.path.join(
                 os.path.dirname(new_filename),
                 '_diff',
